950d0083152adc11f984eb2cf3a271b849ca2207
Kirill Romanov <djaler1@gmail.com>
2017-10-13 14:34:57 +0300

    Calling PostConstruct methods

--- src/main/java/io/datafx/controller/injection/InjectionHandler.java
+++ src/main/java/io/datafx/controller/injection/InjectionHandler.java
@@ -1,21 +1,23 @@
 /**
- * Copyright (c) 2011, 2013, Jonathan Giles, Johan Vos, Hendrik Ebbers All rights reserved. Copyright (c) 2017, Kirill
- * Romanov All rights reserved.
+ * Copyright (c) 2011, 2013, Jonathan Giles, Johan Vos, Hendrik Ebbers All rights reserved. Copyright (c)
+ * 2017, Kirill Romanov All rights reserved.
  * <p>
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
- * following conditions are met: * Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer. * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
- * distribution. * Neither the name of DataFX, the website javafxdata.org, nor the names of its contributors may be used
- * to endorse or promote products derived from this software without specific prior written permission.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided
+ * that the following conditions are met: * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer. * Redistributions in binary form must
+ * reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution. * Neither the name of DataFX, the
+ * website javafxdata.org, nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
  * <p>
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
  */
 package io.datafx.controller.injection;
 
@@ -25,10 +27,12 @@ import io.datafx.controller.context.ViewContext;
 import io.datafx.controller.injection.provider.ContextProvider;
 import io.datafx.core.DataFXUtils;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 
@@ -48,9 +52,19 @@ public class InjectionHandler<U> {
             instance = createNewInstance(propertyClass);
             context.register(instance);
             injectAllSupportedFields(instance);
+            callPostConstructMethods(instance);
         }
 
         return instance;
+    }
+
+    private <T> void callPostConstructMethods(T bean) {
+        Class<T> cls = (Class<T>) bean.getClass();
+        for (Method method : DataFXUtils.getInheritedDeclaredMethods(cls)) {
+            if (method.isAnnotationPresent(PostConstruct.class)) {
+                DataFXUtils.callPrivileged(method, bean);
+            }
+        }
     }
 
     private <T> void injectAllSupportedFields(T bean) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
